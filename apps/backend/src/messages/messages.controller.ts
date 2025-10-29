f011f33b5360bfd5bbdaa1146b3429265e7aa7b7
Gordon Situ <gsitu322@gmail.com>
2025-10-29 14:45:33 -0700

    Added controller method

--- apps/backend/src/messages/messages.controller.ts
+++ apps/backend/src/messages/messages.controller.ts
@@ -2,14 +2,24 @@ import { Controller, Post, Body } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { CreateMessageDto } from "./dto/sendMessage.dto";
 import { MessagesService } from "./messages.service";
+import { MessagesGateway } from "./messages.gateway";
 
 @ApiTags("Messages")
 @Controller("messages")
 export class MessagesController {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(
+    private readonly messagesService: MessagesService,
+    private readonly messagesGatway: MessagesGateway,
+  ) { }
 
   @Post("send")
   async sendMessage(@Body() body: CreateMessageDto) {
     return this.messagesService.sendMessage(body);
+  }
+
+  @Post("thread-created")
+  async threadCreated(@Body() payload: any) {
+    this.messagesGatway.emitThreadCreated(payload);
+    return { ok: true };
   }
 }
